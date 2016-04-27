ef9476ae1455f4ee064e4caa72869b732710f973
Russspruce <russspruce@gmail.com>
2016-04-27 10:54:31 -0700

    Updated rootTest to allow pass for change in text.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -21,7 +21,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Secret Secret");
+    assertThat(pageSource()).contains("Incoming Message");
   }
 
   @Test
